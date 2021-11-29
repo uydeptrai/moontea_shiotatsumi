8425d778dd54ca1cfbb5440a578d92798762c41a
uy <uy.hoang2000214@vnuk.edu.vn>
2021-11-30 02:10:04 +0700

    first commit

---
+++ Assets/Script/Controller Script/MainmenuController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainmenuController : MonoBehaviour
+{
+    public void playGame(){
+        Application.LoadLevel ("Gameplay");
+
+    }
+        
+    }
+}
